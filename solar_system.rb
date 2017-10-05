12db53d65587de03d0c12355c1695a8a04c103a5
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-05 11:35:43 -0400

    defined the add method and @bodies = [] in the initialize mehtod

--- solar_system.rb
+++ solar_system.rb
@@ -2,9 +2,14 @@ class System
 
   attr_accessor :bodies
 
-  def initialize(bodies)
-    @bodies = bodies
-
+  def initialize
+    @bodies = []
   end
+
+  def add(new_body)
+    @bodies << new_body
+  end
+
+
 
 end
