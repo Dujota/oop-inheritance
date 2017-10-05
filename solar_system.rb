825760325b71e2403d66f42698726938318fc5c7
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-05 11:47:42 -0400

    finished add method, finished total mass method, required relative body and created body.rb file curretly empty

--- solar_system.rb
+++ solar_system.rb
@@ -1,3 +1,6 @@
+require_relative 'body'
+
+# First we'll need a class to represent the solar system. Let's call it System, and give it an attribute bodies. bodies will start as an empty array (ie. []).
 class System
 
   attr_accessor :bodies
@@ -6,10 +9,17 @@ class System
     @bodies = []
   end
 
+
+  # Let's give System an instance method called add which will add a celestial body to the list.
   def add(new_body)
     @bodies << new_body
+    @bodies
   end
 
-
-
+  # We'll also need an instance method called total_mass which should add up the mass of all the bodies in bodies, and return it.
+  def total_mass
+    sum = 0
+    @bodies.each{ |body| body[:mass] += sum}
+    sum
+  end
 end
