679baf9272c3562235b54c734e8ea25f26e629eb
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-05 11:49:10 -0400

    defned class for body

---
+++ body.rb
@@ -0,0 +1,5 @@
+# We'll also need a class to represent the various celestial bodies. We'll call it Body. Each of them will need a name and a mass. We'll assign them when we create the body.
+
+class Body
+
+end
